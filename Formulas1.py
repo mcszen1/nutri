a9721342731e0a7ffffd9fdc1266d88db9994f8f
mcszen1 <mcszen@gmail.com>
2023-12-17 10:06:23 -0300

    Update Formulas1.py

--- Formulas1.py
+++ Formulas1.py
@@ -28,9 +28,13 @@ def calcular_necessidades_caloricas(tmb, nivel_atividade):
     # Aqui, você pode adicionar a lógica para calcular as necessidades calóricas com base no TMB e no nível de atividade
     pass
 
-def distribuicao_macronutrientes(calorias):
-    # Aqui, você pode adicionar a lógica para calcular a distribuição de macronutrientes
-    pass
+def calcular_macronutrientes(calorias_diarias):
+    # Exemplo de distribuição: 50% carboidratos, 30% proteínas, 20% gorduras
+    return {
+        'Carboidratos': (calorias_diarias * 0.50) / 4,
+        'Proteínas': (calorias_diarias * 0.30) / 4,
+        'Gorduras': (calorias_diarias * 0.20) / 9
+    }
 st.image("logonutri2.jpg",use_column_width="False")
 st.title('Aplicativo de Avaliação Nutricional')
 
@@ -38,6 +42,9 @@ peso = st.number_input('Peso (kg)', min_value=0.0, format="%.2f")
 altura = st.number_input('Altura (cm)', min_value=0.0, format="%.2f")
 idade = st.number_input('Idade', min_value=0, max_value=120, step=1)
 sexo = st.selectbox('Sexo', ['Masculino', 'Feminino'])
+nivel_atividade = st.selectbox('Nível de Atividade Física', 
+                               ['Sedentário', 'Leve', 'Moderado', 'Ativo', 'Muito Ativo'])
+objetivo = st.selectbox('Objetivo', ['Perder Peso', 'Manter Peso', 'Ganhar Massa'])
 lean_body_mass = st.number_input('Massa Corporal Magra (kg)', min_value=0.0, format="%.2f")
 
 formula_tmb = st.selectbox('Fórmula para TMB', ['Harris-Benedict', 'Mifflin-St Jeor', 'Cunningham', 'Tinsley'])
@@ -55,9 +62,27 @@ if st.button('Calcular'):
             tmb = calcular_tmb_cunningham(lean_body_mass)
         else:
             tmb = calcular_tmb_tinsley(lean_body_mass)
+    fatores_atividade = {'Sedentário': 1.2, 'Leve': 1.375, 'Moderado': 1.55, 'Ativo': 1.725, 'Muito Ativo': 1.9}
+    calorias_manutencao = tmb * fatores_atividade[nivel_atividade]
+    # Ajustando pelas metas
+    if objetivo == 'Perder Peso':
+        calorias_diarias = calorias_manutencao - 500
+    elif objetivo == 'Ganhar Massa':
+        calorias_diarias = calorias_manutencao + 500
+    else:
+        calorias_diarias = calorias_manutencao
 
+    # Calculando macronutrientes
+    macronutrientes = calcular_macronutrientes(calorias_diarias)
     st.write(f"IMC: {imc:.2f}")
     st.write(f"TMB: {tmb:.2f}")
-    # Adicionar apresentação de outras métricas
+    st.write(f"Calorias para Manutenção: {calorias_manutencao:.2f} kcal")
+    st.write(f"Calorias Diárias (com base no objetivo): {calorias_diarias:.2f} kcal")
+    st.write("Distribuição de Macronutrientes:")
+    st.write(f"Carboidratos: {macronutrientes['Carboidratos']:.2f}g")
+    st.write(f"Proteínas: {macronutrientes['Proteínas']:.2f}g")
+    st.write(f"Gorduras: {macronutrientes['Gorduras']:.2f}g")
+
+
 
 # Aqui você pode continuar expandindo o aplicativo conforme necessário.
